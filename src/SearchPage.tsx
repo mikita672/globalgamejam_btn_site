4dfa7a89a36a598532671be6580b15c0eba37d03
Mikita Dzeviatau <9mikita@gmail.com>
2026-01-31 17:57:47 +0100

    refactor: clean up imports and improve formatting in SearchPage component

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from 'react';
+import { useState } from 'react';
 import './SearchPage.css';
 
 // Import assets
@@ -9,42 +9,42 @@ import bush1 from './assets/Bush_1.png';
 import bush2 from './assets/Bush_2.png';
 
 function SearchPage() {
-  const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-  };
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
 
-  return (
-    <div className="search-page">
-      {/* Decorative background elements */}
-      <div className="decorative-elements">
-        <img src={tree1} alt="" className="decorative-element tree-1" />
-        <img src={tree2} alt="" className="decorative-element tree-2" />
-        <img src={tree3} alt="" className="decorative-element tree-3" />
-        <img src={bush1} alt="" className="decorative-element bush-1" />
-        <img src={bush2} alt="" className="decorative-element bush-2" />
-      </div>
+    return (
+        <div className="search-page">
+            {/* Decorative background elements */}
+            <div className="decorative-elements">
+                <img src={tree1} alt="" className="decorative-element tree-1" />
+                <img src={tree2} alt="" className="decorative-element tree-2" />
+                <img src={tree3} alt="" className="decorative-element tree-3" />
+                <img src={bush1} alt="" className="decorative-element bush-1" />
+                <img src={bush2} alt="" className="decorative-element bush-2" />
+            </div>
 
-      {/* Main search content */}
-      <div className="search-content">
-        <h1 className="search-title">Jamm</h1>
+            {/* Main search content */}
+            <div className="search-content">
+                <h1 className="search-title">Jamm</h1>
 
-        <form className="search-form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className="search-input"
-            placeholder="Search for something..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button type="submit" className="search-button">
-            Search
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                <form className="search-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Search for something..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <button type="submit" className="search-button">
+                        Search
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default SearchPage;
